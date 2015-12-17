7ba975b935385ea734e5c6af7186826de81c20ed
rentit <rentit@namy.local>
2015-12-17 09:40:18 +0100

    added labels

--- MyWeather/ViewController.m
+++ MyWeather/ViewController.m
@@ -9,6 +9,15 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (nonatomic, strong) UILabel *cityNameLabel;
+@property (nonatomic, strong) UILabel *currentTempatureLabel;
+@property (nonatomic, strong) UILabel *currentPressureLabel;
+@property (nonatomic, strong) UILabel *currentHumidityLabel;
+@property (nonatomic, strong) UILabel *minTemperatureLabel;
+@property (nonatomic, strong) UILabel *maxTemperatureLabel;
+@property (nonatomic, strong) UILabel *sunriseLabel;
+@property (nonatomic, strong) UILabel *sunsetLabel;
+
 
 @end
 
@@ -17,6 +26,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.cityNameLabel =[UILabel new];
+    self.currentTempatureLabel =[UILabel new];
+    self.currentPressureLabel =[UILabel new];
+    self.currentHumidityLabel =[UILabel new];
+    self.minTemperatureLabel =[UILabel new];
+    self.maxTemperatureLabel =[UILabel new];
+    self.sunriseLabel =[UILabel new];
+    self.sunsetLabel =[UILabel new];
+    
+    [self setupLayout];
+    
+    [self updateLabelsWithData];
+}
+
+-(void) setupLayout{}
+
+- (void) updateLabelsWithData{
+    self.cityNameLabel.text = @"Budapest";
+    self.currentTempatureLabel.text = @"32 C";
+    self.currentPressureLabel.text = @"3532";
+    self.minTemperatureLabel.text = @"12 C";
+    self.maxTemperatureLabel.text = @"35 C";
+    self.sunriseLabel.text = @"5:35AM";
+    self.sunsetLabel.text = @"6:25 AM";
 }
 
 - (void)didReceiveMemoryWarning {
