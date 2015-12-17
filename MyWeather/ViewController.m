ee95f54a989cf15b95d1c964a6f20a785e4615da
rentit <rentit@namy.local>
2015-12-17 11:47:19 +0100

    design labels

--- MyWeather/ViewController.m
+++ MyWeather/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <AFNetworking/AFNetworking.h>
 
 @interface ViewController ()
 @property (nonatomic, strong) UILabel *cityNameLabel;
@@ -42,6 +43,23 @@
 }
 
 -(void) setupLayout{
+
+    self.cityNameLabel.font = [UIFont systemFontOfSize:35];
+    self.cityNameLabel.textAlignment = NSTextAlignmentCenter;
+    
+    self.currentTemperatureLabel.font = [UIFont systemFontOfSize:22];
+    self.currentTemperatureLabel.textAlignment = NSTextAlignmentCenter;
+    
+    self.minTemperatureLabel.font = [UIFont systemFontOfSize:22];
+    self.maxTemperatureLabel.font = [UIFont systemFontOfSize:22];
+    self.currentPressureLabel.font = [UIFont systemFontOfSize:22];
+    self.currentHumidityLabel.font = [UIFont systemFontOfSize:22];
+    
+    self.sunriseLabel.font = [UIFont systemFontOfSize:22];
+    self.sunsetLabel.font = [UIFont systemFontOfSize:22];
+    [self.sunriseLabel setContentHuggingPriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisHorizontal];
+
+    
     self.cityNameLabel.translatesAutoresizingMaskIntoConstraints = NO;
     self.currentTemperatureLabel.translatesAutoresizingMaskIntoConstraints = NO;
     self.currentPressureLabel.translatesAutoresizingMaskIntoConstraints = NO;
@@ -74,7 +92,10 @@
                                                          _sunsetLabel);
     
     [verticalConstraints addObjectsFromArray:
-     [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[_cityNameLabel]-[_currentTemperatureLabel]-[_currentPressureLabel]-[_currentHumidityLabel]-[_minTemperatureLabel]-[_maxTemperatureLabel]-[_sunriseLabel]-[_sunsetLabel]-|" options:0 metrics:nil views:views]];
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-30-[_cityNameLabel(<=50)]-0-[_currentTemperatureLabel(<=20)]-75-[_minTemperatureLabel]-[_maxTemperatureLabel]-[_currentPressureLabel]-[_currentHumidityLabel]-[_sunriseLabel]->=0-|" options:0 metrics:nil views:views]];
+    
+    [verticalConstraints addObjectsFromArray:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:[_currentHumidityLabel]-[_sunsetLabel]" options:0 metrics:nil views:views]];
     
     [horizontalConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_cityNameLabel]-|" options:0 metrics:nil views:views]];
     [horizontalConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_currentTemperatureLabel]-|" options:0 metrics:nil views:views]];
@@ -82,21 +103,32 @@
     [horizontalConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_currentHumidityLabel]-|" options:0 metrics:nil views:views]];
     [horizontalConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_minTemperatureLabel]-|" options:0 metrics:nil views:views]];
     [horizontalConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_maxTemperatureLabel]-|" options:0 metrics:nil views:views]];
-    [horizontalConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_sunriseLabel]-|" options:0 metrics:nil views:views]];
-    [horizontalConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_sunsetLabel]-|" options:0 metrics:nil views:views]];
+    [horizontalConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_sunriseLabel][_sunsetLabel]-|" options:0 metrics:nil views:views]];
     
     [self.view addConstraints:verticalConstraints];
     [self.view addConstraints:horizontalConstraints];
 }
+
+-(void) fetchCurrentWeather {
+    /*NSURL *url = [NSURL URLWithString:@"http://api.openweathermap.org/data/2.5/weather?q=Budapest,hu&appid=2de143494c0b295cca9337e1e96b00e0"];
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    
+    NSURLSessionDataTask *task =
+     */
+    
+}
+
 
 - (void) updateLabelsWithData{
     self.cityNameLabel.text = @"Budapest";
     self.currentTemperatureLabel.text = @"32 C";
     self.currentPressureLabel.text = @"3532";
     self.currentHumidityLabel.text = @"85";
-    self.minTemperatureLabel.text = @"12 C";
-    self.maxTemperatureLabel.text = @"35 C";
-    self.sunriseLabel.text = @"5:35AM";
+    self.minTemperatureLabel.text = @"Minimum temperature today: 12 C";
+    self.maxTemperatureLabel.text = @"Maximum temperature today: 35 C";
+    self.sunriseLabel.text = @"Daylight: 5:35AM - ";
     self.sunsetLabel.text = @"6:25 AM";
 }
 
